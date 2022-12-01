102321221d1a1ac342e2f98650bd23e20ef11974
Steven <stevensmith3211@gmail.com>
2022-12-01 11:37:31 -0600

    added functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    buildTodo(e.target.newTaskDescription.value)
+    form.reset()
+  })
+  
 });
+
+const buildTodo = todo =>{
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent='x'
+  p.textContent= `${todo} `
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+const handleDelete = e =>{
+  e.target.parentNode.remove()
+}
